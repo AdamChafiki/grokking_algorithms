b90a2d516bfbfa62edc79129d0ed29a4c02adce3
Adam Chafiki <adamchafiki3@gmail.com>
2025-08-20 14:04:24 +0100

    tree node

---
+++ tree-node/same-tree.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p, q) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+s1 = Solution()
+s1.isSameTree(
+    TreeNode(1, TreeNode(2), TreeNode(3)), TreeNode(1, TreeNode(2), TreeNode(3))
+)
